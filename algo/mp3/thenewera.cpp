3d2a31fb8cb31e729a4a732f58a5f19f1cda3be9
Minjun Shin <ohgree@u.sogang.ac.kr>
2019-11-28 01:00:52 +0900

    algo mp3 decoding raw byte finished

--- algo/mp3/thenewera.cpp
+++ algo/mp3/thenewera.cpp
@@ -1,4 +1,6 @@
 #define ASCII_MAX_VALUE 127
+#define BYTE_SIZE 8
+#define CODEBLOCK_SIZE (ASCII_MAX_VALUE+1)/BYTE_SIZE
 #define INVALID_CHAR -1
 
 #include <iostream>
@@ -45,9 +47,11 @@ class Huffman {
       void assignBinaryCode(node_ptr, std::string);
       void initialize(void);
       void openFile(void);
-      void writeToFile(std::string);
+      void writeToFile();
       void reconstructTree(void);
       char binToByte(string);
+      string byteToBin(unsigned char*, int);
+      void addToTree(node_ptr);
    public:
       Huffman(std::string, HuffmanOption);
       const string getHuffmanCodeOf(char);
@@ -82,12 +86,12 @@ char Huffman::binToByte(string bin) {
  *  [encoded bit string|0 padding][# of padding]
  *
  */
-void Huffman::writeToFile(std::string bits) {
+void Huffman::writeToFile() {
    string out, cell;
    cout << "[Debug Info]" << endl;
-   cout << bits << endl;
    for(std::vector<node_ptr>::size_type i=0 ; i<huffmanCode.size() ; i++) {
-      cout << huffmanCode[i]->freq << " " << huffmanCode[i]->id << "[" << (int)huffmanCode[i]->id << "]" << huffmanCode[i]->code << endl;
+      cout << huffmanCode[i]->freq << " " <<  huffmanCode[i]->id << 
+         "[" << (int)huffmanCode[i]->id << "]" << huffmanCode[i]->code << endl;
    }
 
    /* open output file in binary mode */
@@ -99,6 +103,7 @@ void Huffman::writeToFile(std::string bits) {
 
    /* first byte in file = number of mappings in huffman code */
    out += (char)huffmanCode.size();
+
    for(std::vector<node_ptr>::size_type i=0 ; i<huffmanCode.size() ; i++) {
       node_ptr node(huffmanCode[i]);
 
@@ -130,14 +135,18 @@ void Huffman::writeToFile(std::string bits) {
    inFile.seekg(0, ios::beg);
    char k;
    string codedBits;
+
+   cout << "raw binary:" << endl;
    while(inFile.good()) {
       inFile.get(k);
       codedBits += getHuffmanCodeOf(k);
       while(codedBits.length() > 8) {
          out += binToByte(codedBits.substr(0, 8));
+         cout << codedBits.substr(0, 8);
          codedBits = codedBits.substr(8);
       }
    }
+   cout << codedBits << endl;
    int num_of_padding = 8-codedBits.length();
    cout << "padding: " << num_of_padding << " to " <<  codedBits << endl;
    codedBits.append(num_of_padding, '0');
@@ -163,6 +172,7 @@ void Huffman::encode(void) {
    char* buffer;
    
    /* read whole file */
+   /*
    inFile.seekg(0, ios::end);
    len = inFile.tellg();
    inFile.seekg(0, ios::beg);
@@ -174,8 +184,9 @@ void Huffman::encode(void) {
    for(int i=0 ; i<len ; i++) {
       encrypted += getHuffmanCodeOf(buffer[i]);
    }
+   */
    
-   writeToFile(encrypted);
+   writeToFile();
 }
 
 void Huffman::openFile(void) {
@@ -191,6 +202,8 @@ void Huffman::initialize(void) {
    if(option == ENCODE) {
       makeHuffmanTree();
       assignBinaryCode(huffmanTree, "");
+   } else {
+      reconstructTree();
    }
 }
 
@@ -261,6 +274,116 @@ void Huffman::makeHuffmanTree(void) {
    pq.pop();
 }
 
+string Huffman::byteToBin(unsigned char* byte, int n) {
+   string result;
+   for(int i=0 ; i<n ; i++) {
+      for(int j=ASCII_MAX_VALUE+1 ; j>0 ; j/=2) {
+         result += (char)(byte[i] / j + '0');
+         byte[i] %= j;
+      }
+   }
+   return result;
+}
+
+void Huffman::addToTree(node_ptr node) {
+   char id = node->id;
+   string code = node->code;
+
+   node_ptr now = this->huffmanTree;
+
+   for(int i=0 ; i < (int)code.size() ; i++) {
+      if(code[i] == '0') {
+         if(!now->left)
+            now->left = new _huffman_node();
+         now = now->left;
+      } else {
+         if(!now->right)
+            now->right = new _huffman_node();
+         now = now->right;
+      }
+   }
+   now->id = id;
+   now->code = string(code);
+}
+
+void Huffman::reconstructTree(void) {
+   int nodeCount, padCount;
+   unsigned char buffer[CODEBLOCK_SIZE];
+   node_ptr temp;
+   nodeCount = inFile.get();
+   cout << "[nodeCount = " << nodeCount << "]" << endl;
+
+   /* initialize root of huffman tree */
+   this->huffmanTree = new _huffman_node();
+
+   /* read headers */
+   for(int i=0 ; i<nodeCount ; i++) {
+      temp = new _huffman_node();
+
+      /* get a id byte from file */
+      temp->id = inFile.get();
+      
+      /* read 128-bit(16Byte) block containing code */
+      inFile.read((char*)buffer, CODEBLOCK_SIZE);
+
+      for(int i=0 ; i<CODEBLOCK_SIZE ; i++)
+         cout << "[" << hex << (int)buffer[i] << "]";
+      cout << endl;
+
+      string codeBit = byteToBin(buffer, CODEBLOCK_SIZE);
+
+      /* count number of 0 paddings in code bits */
+      for(padCount = 0 ; codeBit[padCount] == '0' ; padCount++);
+
+      /* remove 0 paddings and leading 1 from code bits */
+      codeBit = codeBit.substr(padCount+1);
+      temp->code = codeBit;
+
+      cout << "[" << dec << (int)temp->id << " " << temp->code << "]" << endl;
+
+      /* add new node to code list, and add to tree */
+      this->huffmanCode.push_back(temp);
+      addToTree(temp);
+   }
+   inFile.close();
+}
+
+void Huffman::decode() {
+   outFile.open(outFilename, ios::out);
+   inFile.open(inFilename, ios::in|ios::binary);
+   unsigned char mapSize;
+   unsigned char trailing0s;
+   unsigned char temp;
+   vector<unsigned char> text;
+   string decoded;
+   string binary;
+
+   mapSize = inFile.get();
+
+   /* go to the end of the file, get 1 byte: number of trailing zeros */
+   inFile.seekg(-1, ios::end);
+   trailing0s = inFile.get();
+   cout << (int)mapSize << " " << (int)trailing0s << endl;
+
+   /* get to the start of bit stream */
+   inFile.seekg(1 + (CODEBLOCK_SIZE+1)*mapSize, ios::beg);
+
+   temp = inFile.get();
+   while(inFile.good()){
+      text.push_back(temp);
+      temp = inFile.get();
+   }
+   cout << (int)text.back() << endl;
+   text.pop_back(); // remove trailing 1 byte: number of trailing zeros
+
+   binary += byteToBin(&text[0], text.size());
+   binary = binary.substr(0, binary.size()-trailing0s);
+   cout << binary << endl;
+   for(std::vector<unsigned char>::size_type i=0 ; i<text.size()-1 ; i++) {
+   }
+
+}
+
 int main(int argc, const char* argv[]) {
    string filename, arg;
    HuffmanOption opt;
@@ -280,6 +403,10 @@ int main(int argc, const char* argv[]) {
       exit(1);
    }
    Huffman hm(filename, opt);
-   hm.encode();
+
+   if(opt == ENCODE)
+      hm.encode();
+   else
+      hm.decode();
    return 0;
 }
